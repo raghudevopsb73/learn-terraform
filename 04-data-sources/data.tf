25222c7f304a2bc16cb8abb928cb6824e336c1cf
Raghu K <raghuk.vit@gmail.com>
2023-06-07 08:35:50 +0530

    Update learn-terraform/

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -12,3 +12,11 @@ output "price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
 
+data "aws_security_group" "selected" {
+  id = "allow-all"
+}
+
+output "sgid" {
+  value = data.aws_security_group.selected.id
+}
+
