e871c5396970061d972538d611a64e488159fee2
Raghu K <raghuk.vit@gmail.com>
2023-06-07 08:33:14 +0530

    Update learn-terraform/

--- 04-data-sources/data.tf
+++ 04-data-sources/data.tf
@@ -1,6 +1,6 @@
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t3.medium"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   filter {
     name   = "product-description"
