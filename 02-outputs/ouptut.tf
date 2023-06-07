26a4dd56088302918dc0476c7bfd47d914e14144
Raghu K <raghuk.vit@gmail.com>
2023-06-07 07:53:18 +0530

    Update learn-terraform/

---
+++ 02-outputs/ouptut.tf
@@ -0,0 +1,12 @@
+output "sample" {
+  value = "Hello World"
+}
+
+# Any String in terraform need to be quoted with Double quotes only.
+# single quotes are not supported
+
+output "sample1" {
+  value = 1000
+}
+
+# Numbers and booleans (true, false)
