de0fa3bcb47f1be0aa14b110348eb69decff1fcf
Raghu K <raghuk.vit@gmail.com>
2023-06-08 07:41:53 +0530

    Update learn-terraform/

---
+++ 05-resource/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
