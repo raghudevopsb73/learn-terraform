6a47ee8d616f5246a6eb0dbfcc221e42618673aa
Raghu K <raghuk.vit@gmail.com>
2023-06-08 07:53:24 +0530

    Update learn-terraform/

---
+++ 07-module-1/ec2/main.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = var.name
+  }
+}
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+
+resource "aws_security_group" "sg" {
+  name        = var.name
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+variable "name" {}
---
+++ 07-module-1/main.tf
@@ -0,0 +1,9 @@
+module "sample1" {
+  source = "./ec2"
+  name   = "sample1"
+}
+
+module "sample2" {
+  source = "./ec2"
+  name   = "sample2"
+}
