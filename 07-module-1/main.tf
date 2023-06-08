0a8e10c75161d93dc834b87bf4dc5e0942871ebe
Raghu K <raghuk.vit@gmail.com>
2023-06-08 07:59:29 +0530

    Update learn-terraform/

--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -7,3 +7,13 @@ module "sample2" {
   source = "./ec2"
   name   = "sample2"
 }
+
+
+output "public_ip_sample1" {
+  value = module.sample1.public_ip
+}
+
+output "public_ip_sample2" {
+  value = module.sample2.public_ip
+}
+
