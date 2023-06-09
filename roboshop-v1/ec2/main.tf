b58f328bf633883a79f26cc0f7fb662f315a2e98
Raghu K <raghuk.vit@gmail.com>
2023-06-09 08:37:16 +0530

    Update learn-terraform/

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -7,13 +7,17 @@ resource "aws_instance" "web" {
     Name = var.name
   }
 
+}
+
+resource "null_resource" "ansible" {
+  depends_on = [aws_instance.web, aws_route53_record.www]
   provisioner "remote-exec" {
 
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = self.public_ip
+      host     = aws_instance.web.public_ip
     }
 
     inline = [
@@ -21,7 +25,6 @@ resource "aws_instance" "web" {
       "ansible-pull -i localhost, -U https://github.com/raghudevopsb73/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
     ]
   }
-
 }
 
 resource "aws_route53_record" "www" {
