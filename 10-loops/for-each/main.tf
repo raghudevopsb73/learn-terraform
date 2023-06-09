191bd7f00c5afe5542076f366814cbf04dbabd8f
Raghu K <raghuk.vit@gmail.com>
2023-06-09 08:15:48 +0530

    Update learn-terraform/

---
+++ 10-loops/for-each/main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "web" {
+  count         = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = element(var.instances, count.index)
+  }
+}
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = {
+
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.nano"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.nano"
+    }
+
+  }
+}
+
