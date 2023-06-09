f1ea3527a3c1a8533e37e740bed30bb2435357f5
Raghu K <raghuk.vit@gmail.com>
2023-06-09 07:50:09 +0530

    Update learn-terraform/

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -6,3 +6,12 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruit_count" {
+  value = length(var.fruits)
+}
+
